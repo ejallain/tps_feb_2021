5f2d29750ab62590cbae77c83d0defc9c5656b30
ejallain <ejallain@gmail.com>
2022-11-05 11:01:57 -0400

    initial commit

---
+++ notebooks/tps_feb_2021/config.py
@@ -0,0 +1,9 @@
+proj_dir = 'C:/Users/eria/OneDrive - Novozymes A S/Documents/projects/kaggle/tabular_playground_series/tps_feb_2021/'
+
+config = {'data_dir' : proj_dir + 'data/',
+          'models_dir' : proj_dir + 'models/',
+          'proj_dir' : proj_dir
+}
+
+def get_config():
+    return config
---
+++ notebooks/tps_feb_2021/utils.py
@@ -0,0 +1,45 @@
+#set up for TensorBoard
+import os
+import numpy as np
+import pandas as pd
+
+from tps_feb_2021.config import config
+
+root_logdir = config['proj_dir'] + 'notebooks/my_logs'
+
+def get_run_logdir():
+    import time
+    run_id = time.strftime('run_%Y_%m_%d-%H_%M_%S')
+    return os.path.join(root_logdir, run_id)
+
+def save_model(model, name):
+    models_dir = config['models_dir']
+    model.save(models_dir + name)
+    return
+
+def swap_noise(col, p=0.15):
+    '''
+    accepts a data column and probability (p) and returns a
+    'noisy' column where each item has a probability = p of being swapped out
+    for a random element from the column
+    '''
+    from random import random
+
+    noisy_col = []
+    for i in col:
+        if random() <= p:
+            noisy_col.append(np.random.choice(col, 1)[0])
+        else:
+            noisy_col.append(i)
+    return noisy_col
+
+def add_noise(df, p=0.15):
+    '''
+    adds noise to each col of the supplied df according to
+    the supplied probability (p) using the swap_noise function
+    returns a noisified df
+    '''
+    noisy_dict = {}
+    for col in df.columns:
+        noisy_dict[col] = swap_noise(df[col], p)
+    return pd.DataFrame(noisy_dict)
